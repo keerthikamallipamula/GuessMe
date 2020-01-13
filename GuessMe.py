e2efbcb3f705d4fd091fa1d060969110e30a5f66
keerthikamallipamula <meghasaisrikeerthika@gmail.com>
2020-01-13 11:52:36 +0530

    Add reply option.

--- GuessMe.py
+++ GuessMe.py
@@ -2,12 +2,18 @@ import random
 guess_chances = 3
 guess_count = 0
 random_number = random.randint(1, 10)
-while guess_count < guess_chances:
-    guess_number = int(input("Guess number from 1 to 10 : "))
-    guess_count += 1
-    if guess_number == random_number:
-        print("You Win!!!!")
+while True:
+    while guess_count < guess_chances:
+        guess_number = int(input("Guess number from 1 to 10 : "))
+        guess_count += 1
+        if guess_number == random_number:
+            print("You Win!!!!")
+            break
+    else:
+        print(f'Sorry your guess is wrong, The number is {random_number}')
+    try_again=input("Do you want to try again? (Y/N) : ")
+    if try_again.upper() == "N":
         break
-else:
-    print(f'Sorry your guess is wrong, The number is {random_number}')
+    else:
+        guess_count = 0
 
