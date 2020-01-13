bbd745bee8b54096693a33d22838beadaac9e74b
keerthikamallipamula <meghasaisrikeerthika@gmail.com>
2020-01-13 11:42:57 +0530

    My First Commit - Uploading File to Git

---
+++ GuessMe.py
@@ -0,0 +1,13 @@
+import random
+guess_chances = 3
+guess_count = 0
+random_number = random.randint(1, 10)
+while guess_count < guess_chances:
+    guess_number = int(input("Guess number from 1 to 10 : "))
+    guess_count += 1
+    if guess_number == random_number:
+        print("You Win!!!!")
+        break
+else:
+    print(f'Sorry your guess is wrong, The number is {random_number}')
+
